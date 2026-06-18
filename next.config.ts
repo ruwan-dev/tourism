0a13de30d2d2ac9a3a51c155527414df4831b01e
Ruwan <ruwan@jrcsoftware.com>
2026-06-18 16:49:56 +0530

    SSS

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // GitHub Pages වල Image Server එකක් නැති නිසා මේක අනිවාර්යයි
   },
-  basePath: '/tourism', // ඔයාගේ GitHub Repo එකේ නම (CSS/Images කැඩෙන එක නවත්වන්න)
-  assetPrefix: '/tourism/', 
+  //basePath: '/tourism', // ඔයාගේ GitHub Repo එකේ නම (CSS/Images කැඩෙන එක නවත්වන්න)
+  //assetPrefix: '/tourism/', 
 };
 
 export default nextConfig;
