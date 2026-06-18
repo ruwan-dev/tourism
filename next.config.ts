bb59201d943e273bcf4a509d2522c071e5b9c02c
Ruwan <ruwan@jrcsoftware.com>
2026-06-18 16:31:31 +0530

    Configure Next.js for GitHub Pages Static Export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Next.js වලට Static HTML හදන්න කියලා කියනවා
+  images: {
+    unoptimized: true, // GitHub Pages වල Image Server එකක් නැති නිසා මේක අනිවාර්යයි
+  },
+  basePath: '/tourism', // ඔයාගේ GitHub Repo එකේ නම (CSS/Images කැඩෙන එක නවත්වන්න)
+  assetPrefix: '/tourism/', 
 };
 
 export default nextConfig;
